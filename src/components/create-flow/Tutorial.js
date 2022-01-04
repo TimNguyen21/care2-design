d658bfa28353c1c798c523c230d827f1a5957374
TimNguyen21 <tim.nguyen2112@gmail.com>
2022-01-04 11:55:32 -0800

    Intial design for tutorial modal for the petition create flow page

--- src/components/create-flow/Tutorial.js
+++ src/components/create-flow/Tutorial.js
@@ -3,9 +3,21 @@ import '../../scss/create-flow/Tutorial.scss'
 
 const Tutorial = () => {
     return (
-        <section class="create-flow-tutorial">
-            Tutorial
-        </section>
+        <main class="create-flow-tutorial">
+            <div class="create-flow-tutorial__close-button">X</div>
+            <header>HOW TO BUILD A WINNING PETITION</header>
+            <label>It's easy, free, and quick. In just minutes, you be on your way to lasting impact!</label>
+            <div class="create-flow-tutorial__video">
+                <iframe width="535px"
+                        height="300px"
+                        src="https://www.youtube.com/embed/xaPepCVepCg"
+                        title="Care2 Video"
+                        frameBorder="0"
+                        allowFullScreen>
+                </iframe>
+            </div>
+            <div class="create-flow-tutorial__close-label">Ok, I got it, let's go!</div>
+        </main>
     )
 }
 
